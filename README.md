4b334555c2c78545dc960e175109d19806c4b72c
pjbazin <pascal.bazin@gemalto.com>
2017-03-05 18:22:14 +0100

    W3C link

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## Version Status (0.1)
 This code to share very simple web payment apps with one specific feature each.
+Code is collected in a W3C page [examples.md](https://github.com/w3c/webpayments-payment-apps-api/blob/gh-pages/proposals/examples.md)
 
 ## app-bc
 Web payment app handling basic-card method
--- wapps/app-bc.service.js
+++ wapps/app-bc.service.js
@@ -4,8 +4,9 @@
   navigator.serviceWorker
       .register("./app-bc.js", { scope: "./" })
       .then(function(registration) {
+        console.log("[app-bc] registration " + JSON.stringify(registration));
 
-        // Set Registration methods & options
+        // Set Registration methods & options (manifest way)
         if (registration.paymentAppManager) {
           registration.paymentAppManager.setManifest({
             name: "app-bc_key",
@@ -17,35 +18,14 @@
           });
         }
 
-        // Forward registration object to the next function
-        return (registration);
-
-      }).then(function(sw) {
-        // Previous step is successfully completed
-        console.log("[app-bc] Payment-App Service Worker is " + (sw.installing || sw.waiting || sw.active).state);
-
-      }).catch(function(exception) {
-        alert("[app-bc] Dev.Exception: " + exception);
-      });
-}
-
-
-function appbc_register_new() {
-  console.log("[app-bc] Payment-App Service worker registration in progress...");
-
-  navigator.serviceWorker
-      .register('/app-bc.js')
-      .then(function(registration) {
-        console.log("[app-bc] registration " + JSON.stringify(registration));
-
-        // Set Registration methods & options
-        if (registration.paymentAppManager) {
-          registration.paymentAppManager.options.set("app-bc_key", {
-            enabledMethods: ["basic-card"],
-            name: "app-bc for MyCard",
-            id: "M. JAUNE D'EAU;4111111111111111;12;25;987"
-          });
-        }
+        //// Set Registration methods & options
+        //if (registration.paymentAppManager) {
+        //  registration.paymentAppManager.options.set("app-bc_key", {
+        //    enabledMethods: ["basic-card"],
+        //    name: "app-bc for MyCard",
+        //    id: "M. JAUNE D'EAU;4111111111111111;12;25;987"
+        //  });
+        //}
 
         // Forward registration object to the next function
         return (registration);
@@ -58,3 +38,5 @@ function appbc_register_new() {
         alert("[app-bc] Dev.Exception: " + exception);
       });
 }
+
+
